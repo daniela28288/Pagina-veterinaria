416e8caed8a6707098270f68126e8127382b6a67
Daniela Brito <britovanegas@gmail.com>
2024-10-02 12:31:54 -0500

    Cambios de la tarde

---
+++ index.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
