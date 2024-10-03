b752dd4b043d135aff3cb78641b93e1f38f41fd4
Daniela Brito <britovanegas@gmail.com>
2024-10-03 11:01:41 -0500

    Organizacion terminada,cambios de la mañana

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// script.js
-document.getElementById('hamburger').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+// Obtén el icono y el menú
+const menuIcon = document.getElementById('menu-icon');
+const navMenu = document.getElementById('nav-menu');
+
+// Agrega un evento de clic al icono
+menuIcon.addEventListener('click', () => {
+  // Alterna la visibilidad del menú
+  navMenu.style.transform = navMenu.style.transform === 'translateX(0%)' ? 'translateX(100%)' : 'translateX(0%)';
 });
